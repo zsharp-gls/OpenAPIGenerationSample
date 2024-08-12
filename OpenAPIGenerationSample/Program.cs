cea27aeae8f282170cae5d75385842f963bbe335
Zak Sharp <zsharp@greatlakessolutions.com>
2024-08-12 12:08:22 -0500

    add SwaggerDoc information
    Not necessary, but useful if the API will be consumed outside of GLS

--- OpenAPIGenerationSample/Program.cs
+++ OpenAPIGenerationSample/Program.cs
@@ -5,7 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Demo API",
+        Description = "A demo of linking front and back end services with OpenAPI"
+    });
+});
 
 var app = builder.Build();
 
